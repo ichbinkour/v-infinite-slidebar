37290174d1f2c2493662d0f2957495cfd336060f
Valentin Ichkour <valentin.ichkour@epitech.eu>
2020-07-20 16:40:10 +0200

    Initial commit

---
+++ example/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <v-infinite>
+      <h1>Coucou je suis le component</h1>
+    </v-infinite>
+  </div>
+</template>
+
+<script>
+import VInfinite from "../src";
+
+export default {
+  name: "app",
+  components: {
+    VInfinite,
+  },
+};
+</script>
