b3750cab39120f90a6f43833ad720738b54f4f64
Valentin Ichkour <valentin.ichkour@epitech.eu>
2020-07-20 17:51:50 +0200

    Rebuild plugin according to the doc

---
+++ dev/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <v-infinite></v-infinite>
+  </div>
+</template>
+
+<script>
+import VInfinite from "../src/v-infinite";
+
+export default {
+  name: "app",
+  components: {
+    VInfinite,
+  },
+};
+</script>
